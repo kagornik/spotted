06f6d65e944e22107fe5ab443737ae7fc6b89d08
kagornik <karolgornik1997@gmail.com>
2021-04-16 22:01:09 +0200

    Added store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,13 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import authorizationReducer from "../features//counter/authorization/authorizationSlice";
+import spotifyExampleReducer from "../features/spotifyExample/spotifyEampleSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    authorization: authorizationReducer,
+    spotifyExample: spotifyExampleReducer,
   },
 });
 
