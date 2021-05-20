8bd2e06c32345a13ab7d8b06c3586dfa745228ba
kagornik <karolgornik1997@gmail.com>
2021-05-20 19:25:22 +0200

    Adding redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./store";
+import { store, persistor } from "./store";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
 
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,51 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  getDefaultMiddleware,
+  ThunkAction,
+  Action,
+} from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import authorizationReducer from "./features/authorization/authorizationSlice";
 import spotifyExampleReducer from "./features/spotifyExample/spotifyEampleSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-  reducer: {
-    authorization: authorizationReducer,
-    spotifyExample: spotifyExampleReducer,
-  },
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  // whitelist: [],
+};
+
+const reducers = combineReducers({
+  authorization: authorizationReducer,
+  spotifyExample: spotifyExampleReducer,
 });
 
+const presistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: presistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
+
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
